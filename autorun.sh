8e04a8d112c4a8bc01b69cafe5a2b6516d5094d6
wpeguero <wpeguerorosario@outlook.com>
2026-06-14 17:26:25 -0400

    CHANGE: Removed Daily Motion Streams
    The daily motion streams are currently failing, removed them at the moment.

--- autorun.sh
+++ autorun.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo $(dirname $0)
 cd $(dirname $0)/scripts/
-python3 extract_direct_streams.py > ../dailymotion.m3u
+#python3 extract_direct_streams.py > ../dailymotion.m3u
 python3 dlguide.py
 python3 get_streams.py
 echo m3u grabbed
